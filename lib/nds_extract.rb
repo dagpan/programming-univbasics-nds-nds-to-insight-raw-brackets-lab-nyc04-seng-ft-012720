57b85a11bdcd1ce8aa4a85d823bc985650afa61a
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2020-01-20 01:44:16 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -24,6 +24,7 @@ def directors_totals(nds)
   row = 0
   while row < directors_database.length do
       column = 0 
+<<<<<<< HEAD
       sum = 0
       name = directors_database[row][:name]
       while column < directors_database[row][:movies].length do
@@ -37,3 +38,22 @@ def directors_totals(nds)
   pp result_hash
 end
 
+=======
+      result_hash[directors_database[row]] = 0
+      while column < directors_database[row][:movies].length do
+          sum = 0
+#          gros = 0
+#          directors_database[row][:movies][column][:title] = 
+#          sum = sum + directors_database[row][:movies][column][:title]
+#          sum = sum + gros
+#          result_hash << {"#{directors_database[row]}" => sum}
+          result_hash[directors_database[row]] += result_hash[directors_database[row]]
+          puts directors_database[row]
+          column += 1
+      end
+      row += 1
+  end
+  result_hash
+end
+  
+>>>>>>> 2f3c991d8d177b065774bbb1f70d26bec73bb080
